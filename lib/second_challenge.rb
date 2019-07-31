2e35e97ba85a07f918e0691069b8e029b760a7d6
Renee Sims <info@reneesoul.com>
2019-07-31 15:36:21 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,5 +1,5 @@
 def second_challenge
-  epic_tragedy = {
+  epic_tragedy = [
     :montague => {
       :patriarch => {}, 
       :matriarch => {},
@@ -13,7 +13,7 @@ def second_challenge
       :heroine => {},
       :heroine_friends => {}
     }
-    }
+    ]
 
   
 end
